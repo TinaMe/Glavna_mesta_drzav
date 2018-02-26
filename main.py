e93c67fccbf74a3fdfdfd15ded17360cb34497a0
TinaMe <meznaric.tina@gmail.com>
2018-02-26 11:47:34 +0100

    First commit

--- main.py
+++ main.py
@@ -3,10 +3,30 @@
 import os
 import jinja2
 import webapp2
+import random
 
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+
+
+class Drzava:
+    def __init__(self, ime_drzave, glavno_mesto, slika):
+        self.ime_drzave = ime_drzave
+        self.glavno_mesto = glavno_mesto
+        self.slika = slika
+
+
+slovenija = Drzava(ime_drzave="Slovenija", glavno_mesto="Ljubljana", slika="/assets/images/1.jpg")
+avstrija = Drzava(ime_drzave="Avstrija", glavno_mesto="Dunaj", slika="/assets/images/2.jpg")
+nemcija = Drzava(ime_drzave="Nemcija", glavno_mesto="Berlin", slika="/assets/images/3.jpg")
+italija = Drzava(ime_drzave="Italija", glavno_mesto="Rim", slika="/assets/images/4.jpg")
+hrvaska = Drzava(ime_drzave="Hrvaska", glavno_mesto="Zagreb", slika="/assets/images/5.jpg")
+
+drzave_seznam = [slovenija, avstrija, nemcija, italija, hrvaska]
+
+
 
 
 class BaseHandler(webapp2.RequestHandler):
@@ -28,35 +48,39 @@ class BaseHandler(webapp2.RequestHandler):
         return self.response.out.write(template.render(params))
 
 
-class Vstopna(BaseHandler):
+
+class HomeHandler(BaseHandler):
     def get(self):
-        return self.render_template("vstopna.html")
 
-class OMeni(BaseHandler):
-    def get(self):
-        return self.render_template("jaz.html")
+        cifra_drzave = random.randint(0, len(drzave_seznam) - 1)
 
-
-class MojiProjekti(BaseHandler):
-    def get(self):
-        return self.render_template("projekti.html")
+        podatki = {"drzava": drzave_seznam[cifra_drzave].ime_drzave, "mesto": drzave_seznam[cifra_drzave].glavno_mesto, "slika": drzave_seznam[cifra_drzave].slika}
+        return self.render_template("Home.html", podatki)
 
 
-class Blog(BaseHandler):
-    def get(self):
-        return self.render_template("blog.html")
+class ResultHandler(BaseHandler):
+    def post(self):
 
+        vpisano_mesto = self.request.get("mesto")
+        dejanska_drzava = self.request.get("drzava")
+        result = 0
 
-class Kontakt(BaseHandler):
-    def get(self):
-        return self.render_template("kontakt.html")
+        for item in drzave_seznam:
+            if item.ime_drzave == dejanska_drzava:
+                if item.glavno_mesto.lower() == vpisano_mesto.lower():
+                    result = 1
+                    break
+
+        if result == 1:
+            podatki = {"rezultat": "PRAVILEN!"}
+        else:
+            podatki = {"rezultat": "NEPRAVILEN!"}
+        return self.render_template("Result.html", podatki)
+
 
 
 
 app = webapp2.WSGIApplication([
-    webapp2.Route('/', Vstopna),
-    webapp2.Route('/jaz', OMeni),
-    webapp2.Route('/projekti', MojiProjekti),
-    webapp2.Route('/blog', Blog),
-    webapp2.Route('/kontakt', Kontakt),
+    webapp2.Route('/', HomeHandler),
+    webapp2.Route('/Result', ResultHandler),
 ], debug=True)
